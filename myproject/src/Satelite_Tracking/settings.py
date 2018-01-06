c3cc17a4b7ae75290d517ef1cdcf6944eff138b6
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2018-01-06 11:00:16 +0100

    add ajax csrf error fix

--- myproject/src/Satelite_Tracking/settings.py
+++ myproject/src/Satelite_Tracking/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'Sat_Track',
     'kronos',
     'django_countries',
@@ -46,12 +47,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True 
 
 ROOT_URLCONF = 'Satelite_Tracking.urls'
 
